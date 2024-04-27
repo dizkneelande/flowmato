2544254fcd9b95c95d286223e2557e17a8eb8771
Pirateness <notpirateness@gmail.com>
2024-04-27 18:34:39 +1000

    timer barebones logic

--- src/main/java/com/example/flowmato/controller/MainApplicationController.java
+++ src/main/java/com/example/flowmato/controller/MainApplicationController.java
@@ -5,8 +5,37 @@ import javafx.scene.control.Label;
 
 public class MainApplicationController {
 
+    TimerController timer;
     @FXML
     private Label welcomeLabel;
+
+    @FXML
+    protected void StartTimer() {
+        System.out.println("Starting timer");
+        timer.Resume();
+    }
+
+    @FXML
+    protected void GetTime() {
+        System.out.println(timer.timerDuration * 60 - timer.getTimeElapsed());
+    }
+
+    @FXML
+    protected void PauseTimer() {
+        System.out.println("Pausing timer");
+        timer.Pause();
+    }
+
+    @FXML
+    protected void StopTimer() {
+        System.out.println("Stopping timer");
+        timer.Stop();
+    }
+
+    public MainApplicationController() {
+        System.out.println("Initialising timer");
+        timer = new TimerController();
+    }
 
     //main app stuff goes here
 }
---
+++ src/main/java/com/example/flowmato/controller/TimerController.java
@@ -0,0 +1,148 @@
+package com.example.flowmato.controller;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerController {
+    int timerDuration;
+    int sessionDuration;
+    int shortBreakDuration;
+    int longBreakDuration;
+    int currentStage;
+    int shortBreaksTaken;
+    boolean isPaused;
+    private int timeElapsed;
+    private Timer timer;
+    private TimerTask task;
+    private Instant startTime;
+    private Instant pauseTime;
+
+    public TimerController() {
+        sessionDuration = 150;
+        shortBreakDuration = 600;
+        longBreakDuration = 1800;
+        currentStage = 0;
+        timeElapsed = 0;
+        timerDuration = sessionDuration;
+
+        timer = new Timer();
+
+        isPaused = true;
+    }
+
+    public void Stop() {
+        Reset();
+
+        currentStage = 0;
+    }
+
+    private void Reset() {
+        isPaused = true;
+
+        task.cancel();
+        timeElapsed = 0;
+
+        startTime = null;
+        pauseTime = null;
+        timerDuration = sessionDuration;
+    }
+
+    public void Pause() {
+        isPaused = true;
+
+        pauseTime = Instant.now();
+
+        task.cancel();
+    }
+
+    public boolean setSessionDuration(int sessionDuration) {
+        if (!isPaused) {
+            return false;
+        }
+
+        this.sessionDuration = sessionDuration;
+        return true;
+    }
+
+    public boolean setShortBreakDuration(int shortBreakDuration) {
+        if (!isPaused) {
+            return false;
+        }
+
+        this.shortBreakDuration = shortBreakDuration;
+        return true;
+    }
+
+    public boolean setLongBreakDuration(int longBreakDuration) {
+        if (!isPaused) {
+            return false;
+        }
+
+        this.longBreakDuration = longBreakDuration;
+        return true;
+    }
+
+    public int getTimeElapsed() {
+        if (startTime == null) {
+            return 0;
+        } else {
+            if (isPaused) {
+                return (int) Duration.between(startTime, pauseTime).toMillis() + timeElapsed;
+            } else {
+                if (pauseTime != null && pauseTime.isAfter(startTime)) {
+                    return (int) (Duration.between(pauseTime, Instant.now()).toMillis() + timeElapsed);
+                } else {
+                    return (int) (Duration.between(startTime, Instant.now()).toMillis() + timeElapsed);
+                }
+            }
+        }
+    }
+
+    public void Resume() {
+        if ((pauseTime != null) && (pauseTime.isAfter(startTime))) {
+            timeElapsed = (int) (Duration.between(startTime, pauseTime).toMillis() + timeElapsed);
+            startTime = Instant.now();
+        }
+
+        if (startTime == null) {
+            startTime = Instant.now();
+        }
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Timer finished!");
+                nextStage();
+            }
+        };
+
+        System.out.println("TIME LEFT:");
+        System.out.println((timerDuration * 60) - timeElapsed);
+        System.out.println("TIMER START:");
+        System.out.println(startTime);
+        timer.schedule(task, (timerDuration * 60L) - timeElapsed);
+
+        isPaused = false;
+    }
+
+    public void nextStage() {
+        Reset();
+
+        currentStage++;
+        System.out.println(currentStage);
+
+        if (currentStage % 2 != 0) {
+            if (shortBreaksTaken % 4 == 0) {
+                timerDuration = longBreakDuration;
+            } else {
+                timerDuration = shortBreakDuration;
+                shortBreaksTaken++;
+            }
+        } else {
+            timerDuration = sessionDuration;
+        }
+
+        Resume();
+    }
+}
