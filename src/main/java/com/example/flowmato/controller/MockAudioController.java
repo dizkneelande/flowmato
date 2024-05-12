c57340ae5d3a3eae652b32a826a9cb985406d2d0
Pirateness <39227245+Pirateness@users.noreply.github.com>
2024-05-12 23:33:07 +1000

    added blank constructor

--- src/main/java/com/example/flowmato/controller/MockAudioController.java
+++ src/main/java/com/example/flowmato/controller/MockAudioController.java
@@ -1,6 +1,9 @@
 package com.example.flowmato.controller;
 
 public class MockAudioController extends AudioController {
+    public MockAudioController() {
+
+    }
     @Override
     public void playNotification() {
         return;
