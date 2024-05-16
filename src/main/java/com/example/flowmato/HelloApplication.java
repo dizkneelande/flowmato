eba52bf6ea97fc7eff0958975eb63d09991b8081
Voltadox <jakegee0@gmail.com>
2024-05-16 12:56:53 +1000

    Flowmato icon added to window and taskbar

--- src/main/java/com/example/flowmato/HelloApplication.java
+++ src/main/java/com/example/flowmato/HelloApplication.java
@@ -5,10 +5,12 @@ import com.example.flowmato.controller.NotificationController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import com.example.flowmato.model.SqliteProfileDAO;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     public static final String TITLE = "Flowmato";
@@ -21,6 +23,13 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 360);
         stage.setTitle(TITLE);
+
+        // Set the icon to Flowmato.png
+        stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("/images/Flowmato.png"))));
+
+        stage.setScene(scene);
+        stage.show();
+
         stage.setScene(scene);
         stage.show();
         SqliteProfileDAO dao = new SqliteProfileDAO();
