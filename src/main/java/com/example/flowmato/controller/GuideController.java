26caa54e5687bcac70d55d6b611536530d98834a
Nathaniel Mason <n11412402@qut.edu.au>
2024-05-21 08:56:51 +1000

    added functionality

--- src/main/java/com/example/flowmato/controller/GuideController.java
+++ src/main/java/com/example/flowmato/controller/GuideController.java
@@ -1,4 +1,15 @@
 package com.example.flowmato.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class GuideController {
+    @FXML
+    Button exitGuideButton;
+    @FXML
+    protected void exitGuide() {
+        Stage stage = (Stage) exitGuideButton.getScene().getWindow();
+        stage.close();
+    }
 }
