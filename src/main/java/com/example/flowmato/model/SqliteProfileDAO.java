abaaa86fb267833791ae39fdddf10fbdf6bbd87d
Maanvi Gharge <maanvigharge@icloud.com>
2024-05-25 19:14:40 +0530

    Updated SqlLite achievements

--- src/main/java/com/example/flowmato/model/SqliteProfileDAO.java
+++ src/main/java/com/example/flowmato/model/SqliteProfileDAO.java
@@ -132,7 +132,7 @@ public class SqliteProfileDAO {
     private void createMockAchievements(int profileId) {
         Achievements firstTomato = new Achievements(profileId, "First Tomato", LocalDateTime.now().minusDays(5), getClass().getResource("/images/firstmato.png").toString());
         Achievements overgrown = new Achievements(profileId, "Overgrown", LocalDateTime.now().minusMinutes(10), getClass().getResource("/images/weed.png").toString());
-        Achievements procrastinator = new Achievements(profileId, "Procrastinator", LocalDateTime.now().minusDays(1), getClass().getResource("/images/sriracha.png").toString());
+        Achievements procrastinator = new Achievements(profileId, "Procrastinator", LocalDateTime.now().minusDays(1), getClass().getResource("/images/proc.png").toString());
         Achievements lateNightHero = new Achievements(profileId, "Late Night Hero", LocalDateTime.now().minusDays(2).withHour(0).withMinute(30), getClass().getResource("/images/moist.png").toString());
         Achievements speedrunner = new Achievements(profileId, "Speedrunner", LocalDateTime.now().minusDays(4), getClass().getResource("/images/rat.png").toString());
 
