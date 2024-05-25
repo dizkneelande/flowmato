12393d9a88050309ed1b7c7f73cf29dbae7faf44
Maanvi Gharge <maanvigharge@icloud.com>
2024-05-25 19:37:58 +0530

    Final edit to achievements

--- src/main/java/com/example/flowmato/model/SqliteProfileDAO.java
+++ src/main/java/com/example/flowmato/model/SqliteProfileDAO.java
@@ -134,13 +134,13 @@ public class SqliteProfileDAO {
         Achievements partyTime = new Achievements(profileId, "Party Time!", LocalDateTime.now().minusMinutes(10), getClass().getResource("src/main/resources/images/party.png").toString());
         Achievements procrastinator = new Achievements(profileId, "Procrastinator", LocalDateTime.now().minusDays(1), getClass().getResource("/images/proc.png").toString());
         Achievements lateNightHero = new Achievements(profileId, "Late Night Hero", LocalDateTime.now().minusDays(2).withHour(0).withMinute(30), getClass().getResource("src/main/resources/images/Latenight.png").toString());
-        Achievements speedrunner = new Achievements(profileId, "Speedrunner", LocalDateTime.now().minusDays(4), getClass().getResource("/images/rat.png").toString());
+        Achievements overgrown = new Achievements(profileId, "Overgrown", LocalDateTime.now().minusDays(4), getClass().getResource("src/main/resources/images/overgrown.png").toString());
 
         saveAchievement(firstTomato);
         saveAchievement(partyTime);
         saveAchievement(procrastinator);
         saveAchievement(lateNightHero);
-        saveAchievement(speedrunner);
+        saveAchievement(overgrown);
     }
 
     /**
