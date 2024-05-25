dc668134aab629749be5cb554df0d304eca66482
Pirateness <39227245+Pirateness@users.noreply.github.com>
2024-05-25 17:21:11 +1000

    slight code cleanup

--- src/main/java/com/example/flowmato/model/Notification.java
+++ src/main/java/com/example/flowmato/model/Notification.java
@@ -27,9 +27,9 @@ public class Notification {
      * @param displayTime the amount of time to display the notification for (in millis)
      */
     public Notification (String type, String message, String position, int displayTime) {
-        this.type = type;
+        setType(type);
         this.message = message;
-        this.position = position;
+        setPosition(type, position);
         this.displayTime = displayTime / 1000;
 
         if (displayTime == 0) {
@@ -44,12 +44,12 @@ public class Notification {
      * @param displayTime the amount of time to display the notification for (in millis)
      */
     public Notification (String type, String message, int displayTime) {
-        if (type.equals("banner")) {
+        setType(type);
+        if (type.equals("banner") || type.equals("alert")) {
             this.position = "TOP";
         } else {
             this.position = "TOP_RIGHT";
         }
-        this.type = type;
         this.message = message;
         this.displayTime = displayTime / 1000;
 
@@ -64,5 +64,54 @@ public class Notification {
      */
     public void setDisplayTime(int displayTime) {
         this.displayTime = displayTime / 1000;
+    }
+
+    /**
+     * Verifies and sets the type of the notification
+     * @param type the type of notification, which can be <b>"toast"</b>, <b>"banner"</b> or <b>"alert"</b>
+     */
+    private void setType(String type) {
+        switch (type) {
+            case "banner":
+            case "alert":
+            case "toast":
+                this.type = type;
+                break;
+            default:
+                System.out.println("Invalid type selected, falling back to default type BANNER");
+                this.type = "banner";
+                break;
+        }
+    }
+
+    /**
+     * Verifies and sets the position of the notification
+     * @param type the type of notification, which can be <b>"toast"</b>, <b>"banner"</b> or <b>"alert"</b>
+     * @param position the position to render the notification at (check Notification.java for valid values)
+     */
+    public void setPosition(String type, String position) {
+        switch (position) {
+            case "TOP_LEFT":
+            case "TOP_RIGHT":
+            case "BOTTOM_LEFT":
+            case "BOTTOM_RIGHT":
+            case "TOP":
+            case "BOTTOM":
+                this.position = position;
+                return;
+            default:
+                System.out.println("Invalid position provided, falling back to default position for " + type);
+                switch (type) {
+                    case "banner":
+                    case "alert":
+                        this.position = "TOP";
+                        return;
+                    case "toast":
+                        this.position = "TOP_RIGHT";
+                        return;
+                    default:
+                        break;
+                }
+        }
     }
 }
